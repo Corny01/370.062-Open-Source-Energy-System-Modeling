f902595278c0929f535d3a84d84e34bc7619ddf3
Corny <cornelius.urff01@gmail.com>
2024-04-08 19:18:09 +0200

    create mathematical_operations.py ans test_mathematical_opreations.py

---
+++ mathematical_operations.py
@@ -0,0 +1,34 @@
+def add(a, b):
+    """adds two numbers"""
+    return a + b
+
+def subtract(a, b):
+    """subtracts two numbers"""
+    return a - b
+
+def multiply(a, b):
+    """multiplies two numbers"""
+    return a * b
+
+def divide(a, b):
+    """divides two numbers"""
+    if b != 0:
+        return a / b
+    else:
+        return "Division by zero is not allowed!"
+
+# data input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# calculations
+result_add = add(num1, num2)
+result_subtract = subtract(num1, num2)
+result_multiply = multiply(num1, num2)
+result_divide = divide(num1, num2)
+
+# displays results2
+print("Addition:", result_add)
+print("Subtraction:", result_subtract)
+print("Multiplication:", result_multiply)
+print("Division:", result_divide)
---
+++ test_mathematical_operations.py
@@ -0,0 +1,11 @@
+from mathematical_operations import add
+from mathematical_operations import subtract
+from mathematical_operations import multiply
+from mathematical_operations import divide
+
+def test_mathematical_operations():
+    assert add(1,2 ) == 3
+    assert subtract(1, 2) == -1
+    assert multiply(1, 2) == 2
+    assert divide(1, 2) == 0.5
+    assert divide(1, 0) == "Division by zero is not allowed!"
